e0c5a70f69f22492bf60cf697f0320e4c4115f21
Liviu Trifoi <trifoi@gmail.com>
2019-11-11 22:10:55 +0200

    add training data

---
+++ RentEstimator/RentEstimator/RentEstimator/HouseData.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentEstimator
+{
+    class HouseData
+    {
+        [LoadColumn(1)]
+        public string TransactionDate;
+
+        [LoadColumn(2)]
+        public float HouseAge;
+
+        [LoadColumn(3)]
+        public float DistanceToMetro;
+
+        [LoadColumn(4)]
+        public int NumberOfNearbyShops;
+
+        [LoadColumn(5)]
+        public float Latitude;
+
+        [LoadColumn(6)]
+        public string Longitude;
+
+        [LoadColumn(7)]
+        public string Price;
+    }
+}
---
+++ RentEstimator/RentEstimator/RentEstimator/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.ML;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentEstimator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoadData();
+            Console.WriteLine("hello world");
+            Console.ReadKey();
+        }
+
+        public static void LoadData()
+        {
+            var mlContext = new MLContext();
+
+            string TrainDataPath = @".\data\house-prices.csv";
+            string TestDataPath = @".\data\test-prices.csv";
+
+            var trainDataView = mlContext.Data.LoadFromTextFile<HouseData>(TrainDataPath, hasHeader: true, separatorChar: ',');
+            var testDataView = mlContext.Data.LoadFromTextFile<HouseData>(TestDataPath, hasHeader: true, separatorChar: ',');
+
+            var preview = trainDataView.Preview(4);
+        }
+    }
+}
